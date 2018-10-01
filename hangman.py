44366ad2683e6fb3d44a3109e28da86ac6e33796
carissatang <43019204+carissatang@users.noreply.github.com>
2018-10-01 09:30:43 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,2 +1,10 @@
 # hangman.py 
-    
+
+def display_letters(word, guesses):
+    """Displays (prints) the letter-board,
+    underscores (_) for letters not yet guessed.
+    Args:
+        word (str): The secret word
+        guesses (str): A string of correct guesses
+    """
+    pass
